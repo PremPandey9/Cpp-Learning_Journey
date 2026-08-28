8c625ef470d67714f035f738382892e09a750bf9
PremPandey9 <prem.25bsa10193@vitbhopal.ac.in>
2026-08-29 00:38:36 +0530

    Add first C++ project

---
+++ Block_Latter.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+  std::cout << "       PPPP   RRRR   EEEEE  M   M        \n";
+  std::cout << "       P   P  R   R  E      MM MM        \n";
+  std::cout << "       P   P  R   R  E      MM MM        \n";
+  std::cout << "       PPPP   RRRR   EEEEE  M M M        \n";
+  std::cout << "       P      R R    E      M   M        \n";
+  std::cout << "       P      R  R   E      M   M        \n";
+  std::cout << "       P      R   R  EEEEE  M   M        \n";
+  std::cout << "\n";
+  std::cout << "PPPP     A    N   N  DDDD   EEEEE  Y   Y\n";
+  std::cout << "P   P   A A   NN  N  D   D  E      Y   Y\n";
+  std::cout << "P   P  A   A  N N N  D   D  E       Y Y \n";
+  std::cout << "PPPP   AAAAA  N  NN  D   D  EEEEE    Y  \n";
+  std::cout << "P      A   A  N   N  D   D  E        Y  \n";
+  std::cout << "P      A   A  N   N  D   D  E        Y  \n";
+  std::cout << "P      A   A  N   N  DDDD   EEEEE    Y  \n";
+}

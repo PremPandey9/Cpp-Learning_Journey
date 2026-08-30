37d46d0f378d27aae5f226cd24d059a52aef8ce2
PremPandey9 <prem.25bsa10193@vitbhopal.ac.in>
2026-08-30 23:49:06 +0530

    Improve Error

--- Harry Potter — Sorting Hat Quiz/sorting_hat.cpp
+++ Harry Potter — Sorting Hat Quiz/sorting_hat.cpp
@@ -6,6 +6,11 @@ int main(){
     int Hufflepuff;
     int Slytherin;
 
+    Gryffindor = 0
+    Ravenclaw = 0
+    Hufflepuff = 0
+    Slythe = 0
+
     int one_ques;
     std::cout << "Do you like Dawn or Dusk?\n";
     std::cout << "1. Dawn\n";
@@ -17,19 +22,20 @@ int main(){
     {
     case 1:
         std::cout << "You Select Dawn\n";
-        Gryffindor == +1;
+        Gryffindor == 1;
         break;
     case 2:
         std::cout << "You Select Dusk\n";
-        Slytherin == +1;
+        Slytherin == 1;
+        break;
     }
 
     int two_ques;
     std::cout << "When I'm dead, I want people to remember me as: \n";
     std::cout << "1. The Good\n";
     std::cout << "2. The Great\n";
-    std::cout << "1. The Wise\n";
-    std::cout << "2. The Bold\n";
+    std::cout << "3. The Wise\n";
+    std::cout << "4. The Bold\n";
 
     std::cout << "Enter Your Choice: ";
     std::cin >> two_ques;
@@ -37,28 +43,28 @@ int main(){
     {
     case 1:
         std::cout << "The Good Person\n";
-        Hufflepuff == +1;
+        Hufflepuff += 1;
         break;
     case 2:
         std::cout << "The Great Person\n";
-        Slytherin == +1;
-    default:
+        Slytherin += 1;
         break;
     case 3:
         std::cout << "The Wise Person\n";
-        Ravenclaw == +1;
+        Ravenclaw += 1;
         break;
     case 4:
         std::cout << "The Bold Person\n";
-        Gryffindor == +1;
+        Gryffindor += 1;
+        break;
     }
 
     int third_ques;
     std::cout << "Which kind of instrument most pleases your ear? \n";
     std::cout << "1. The Violin\n";
     std::cout << "2. The Trumpet\n";
-    std::cout << "1. The Piano\n";
-    std::cout << "2. The Drum\n";
+    std::cout << "3. The Piano\n";
+    std::cout << "4. The Drum\n";
 
     std::cout << "Enter Your Choice: ";
     std::cin >> third_ques;
@@ -66,27 +72,27 @@ int main(){
     {
     case 1:
         std::cout << "I Like Most Violin\n";
-        Slytherin == +1;
+        Slytherin += 1;
         break;
     case 2:
         std::cout << "I Like Most Trumpet\n";
-        Hufflepuff == +1;
-    default:
+        Hufflepuff += 1;
         break;
     case 3:
         std::cout << "I Like Most Piano\n";
-        Ravenclaw == +1;
+        Ravenclaw += 1;
         break;
     case 4:
         std::cout << "I Like Most Drum\n";
-        Gryffindor == +1;
+        Gryffindor += 1;
+        break;
     }
     int fourth_ques;
     std::cout << "Which road tempts you most? \n";
     std::cout << "1. The wide, sunny, grassy lane\n";
     std::cout << "2. The narrow, dark, lantern-lit alley\n";
-    std::cout << "1. The twisting, leaf-strewn path through woods\n";
-    std::cout << "2. The cobbled street lined with ancient buildings\n";
+    std::cout << "3. The twisting, leaf-strewn path through woods\n";
+    std::cout << "4. The cobbled street lined with ancient buildings\n";
 
     std::cout << "Enter Your Choice: ";
     std::cin >> fourth_ques;
@@ -94,19 +100,19 @@ int main(){
     {
     case 1:
         std::cout << "The wide, sunny, grassy lane\n";
-        Slytherin == +1;
+        Slytherin += 1;
         break;
     case 2:
         std::cout << "The narrow, dark, lantern-lit alley\n";
-        Hufflepuff == +1;
-    default:
+        Hufflepuff += 1;
         break;
     case 3:
         std::cout << "The twisting, leaf-strewn path through woods\n";
-        Ravenclaw == +1;
+        Ravenclaw += 1;
         break;
     case 4:
         std::cout << "The cobbled street lined with ancient buildings\n";
-        Gryffindor == +1;
+        Gryffindor += 1;
+        break;
     }
 }

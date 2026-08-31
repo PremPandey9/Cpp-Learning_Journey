1784551611456aca129b3283d345ab14f7207ef1
PremPandey9 <prem.25bsa10193@vitbhopal.ac.in>
2026-09-01 03:13:56 +0530

    Project Completed

--- FIZZ BUZZ/fizz_buzz.cpp
+++ FIZZ BUZZ/fizz_buzz.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
 int main(){
+    int number;
+
+    for (number = 1; number <= 100; number++)
+    {
+        if (number % 5 == 0 && number % 3 == 0)
+        {
+            std::cout << "FizzBuzz\n";
+        }
+        else if (number % 5 == 0)
+        {
+            std::cout << "Buzz\n";
+        }
+        else if (number % 3 == 0)
+        {
+            std::cout << "Fizz\n";
+        }
+        else{
+            std::cout << number << "\n";
+        }
+        
+    }
     
 }
